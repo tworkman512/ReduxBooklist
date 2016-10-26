8c5bab9ee934a7d355a9a0baf0e8fd8fe06c4a06
tom-workman <tom.workman@us.ibm.com>
2016-10-26 12:17:08 -0500

    Initial commit

---
+++ src/containers/book_detail.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+    if (!this.props.book) {
+      return (
+        <div>
+          Welcome! Select a book to get started.
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Author: {this.props.book.author}</div>
+        <div>Pages: {this.props.book.pages}</div>
+        <div>Description: {this.props.book.description}</div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', pages: 172, description: "Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that's more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code." },
+    { title: 'Extreme Ownership', author: 'Jocko Willink & Leif Babin', pages: 320, description: "A leadership book that demonstrates how to truly lead, taken from lessons learned on the battlefield and applied in the world of business."},
+    { title: 'Eloquent JavaScript', author: 'Marijn Haverbeke', pages: 224, description: "Eloquent JavaScript goes beyond the cut-and-paste scripts of the recipe books and teaches you to write code that's elegant and effective. You'll start with the basics of programming, and learn to use variables, control structures, functions, and data structures. Then you'll dive into the real JavaScript artistry: higher-order functions, closures, and object-oriented programming."},
+    { title: 'Benjamin Franklin: An American Life', author: 'Walter Isaacson', pages: 586, description: "In this authoritative and engrossing full-scale biography, Walter Isaacson, bestselling author of Einstein and Steve Jobs, shows how the most fascinating of America's founders helped define our national character."}
+  ];
+}
